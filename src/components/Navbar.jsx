6b0718edd0339a61d9a849a30e9a95f5497b4e25
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-16 11:16:58 +0200

    add state to toggle the navbar open and close

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const openNav = () => {
+    setIsNavOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
     <nav>
-      <div className="flex justify-between items-center w-full max-w-[133rem] h-auto py-11 px-8 mx-auto absolute top-0 left-0 right-0 z-50">
-        <div className="w-[14.5rem]">
+      <div className="flex justify-between items-center w-full max-w-[133rem] h-auto py-7 px-5 mx-auto absolute top-0 left-0 right-0 z-50">
+        <div className="w-[145px]">
           <Link to="/">
             <img
               src="./images/logo/logo.png"
@@ -13,12 +24,22 @@ const Navbar = () => {
             />
           </Link>
         </div>
-        <div className="flex text-[2.8rem] cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]">
+        <div
+          onClick={openNav}
+          className="flex text-3xl cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]"
+        >
           <i className="fa-solid fa-bars"></i>
         </div>
       </div>
-      <div className="flex justify-center items-center bg-white w-full h-screen fixed top-0 left-0 transition-all duration-500 ease-in-out z-[999]">
-        <div className="absolute top-10 right-10 text-3xl cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]">
+      <div
+        className={`flex justify-center items-center bg-white w-full h-screen fixed top-0 transition-all duration-500 ease-in-out z-[999] ${
+          isNavOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <div
+          onClick={closeNav}
+          className="absolute top-8 right-8 text-3xl cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]"
+        >
           <i className="fa-solid fa-xmark"></i>
         </div>
         <ul className="flex flex-col text-2xl gap-8 text-center">
