8c11d54a7a8c013228aee48f8ac4058c90b80a8d
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-16 11:07:27 +0200

    add navbar for mobile screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,14 +13,67 @@ const Navbar = () => {
             />
           </Link>
         </div>
-        <div className="flex text-[2.8rem] cursor-pointer transition-all duration-300 hover:text-[#ff4d30]">
+        <div className="flex text-[2.8rem] cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]">
           <i className="fa-solid fa-bars"></i>
         </div>
+      </div>
+      <div className="flex justify-center items-center bg-white w-full h-screen fixed top-0 left-0 transition-all duration-500 ease-in-out z-[999]">
+        <div className="absolute top-10 right-10 text-3xl cursor-pointer transition-colors duration-300 hover:text-[#ff4d30]">
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+        <ul className="flex flex-col text-2xl gap-8 text-center">
+          <li>
+            <Link
+              to="/"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="models"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              Models
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="testimonials"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              Testimonials
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="our-team"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              Our Team
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              className="text-[#010103] font-medium transition-colors duration-300 hover:text-[#ff4d30]"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 };
-
-
 
 export default Navbar;
