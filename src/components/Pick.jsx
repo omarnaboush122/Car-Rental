4e8d2eb3b6c2e9c896ab62b2a89a2cfb04b117af
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-18 10:44:18 +0200

    add plan data to json file then refactor the plan section

---
+++ src/components/Pick.jsx
@@ -0,0 +1,11 @@
+
+
+const Pick = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Pick;
--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,53 +1,30 @@
+import data from "../../data.json";
+
 const Plan = () => {
   return (
     <section className="bg-[#fff] py-14">
       <div className="max-w-[1480px] mx-auto px-8">
         <div className="flex flex-col">
           <div className="text-Black mx-auto text-center">
-            <h3 className="font-Rubik text-2xl font-medium">
-              Plan your trip now
-            </h3>
+            <h3 className="font-Rubik text-2xl font-medium">{data.title}</h3>
             <h2 className="font-Rubik text-5xl font-bold mt-4 mb-8">
-              Quick & easy car rental
+              {data.subtitle}
             </h2>
           </div>
           <div className="grid grid-cols-1 gap-5 px-8 mt-9 md:grid-cols-2 lg:grid-cols-3">
-            <div className="p-3 text-center">
-              <img
-                src="./images/plan/icon1.png"
-                alt="icon-img"
-                className="w-44 mx-auto h-auto"
-              />
-              <h3 className="text-2xl font-bold mb-3">Select Car</h3>
-              <p className="text-[#706f7b] font-Rubik max-w-xs mx-auto">
-                We offers a big range of vehicles for all your driving needs. We
-                have the perfect car to meet your needs
-              </p>
-            </div>
-            <div className="p-3 text-center">
-              <img
-                src="./images/plan/icon2.png"
-                alt="icon-img"
-                className="w-44 mx-auto h-auto"
-              />
-              <h3 className="text-2xl font-bold mb-3">Contact Operator</h3>
-              <p className="text-[#706f7b] font-Rubik max-w-xs mx-auto">
-                Our knowledgeable and friendly operators are always ready to
-                help with any questions or concerns
-              </p>
-            </div>
-            <div className="p-3 text-center">
-              <img
-                src="./images/plan/icon3.png"
-                alt="icon-img"
-                className="w-44 mx-auto h-auto"
-              />
-              <h3 className="text-2xl font-bold mb-3">Let's Drive</h3>
-              <p className="text-[#706f7b] font-Rubik max-w-xs mx-auto">
-                Whether you're hitting the open road, we've got you covered with
-                our wide range of cars
-              </p>
-            </div>
+            {data.items.map((item, i) => (
+              <article key={i} className="p-3 text-center">
+                <img
+                  src={item.icon}
+                  alt="icon-img"
+                  className="w-44 mx-auto h-auto"
+                />
+                <h3 className="text-2xl font-bold mb-3">{item.title}</h3>
+                <p className="text-[#706f7b] font-Rubik max-w-xs mx-auto">
+                  {item.description}
+                </p>
+              </article>
+            ))}
           </div>
         </div>
       </div>
