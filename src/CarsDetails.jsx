a4e54859613ef9ec9061d565f52d4716066f23ca
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-18 17:42:08 +0200

    refactor pick section by moving the cars details to seperate file

---
+++ src/CarsDetails.jsx
@@ -0,0 +1,60 @@
+
+
+const cars = {
+  audi: {
+    img: "audia1",
+    price: 45,
+    model: "Audi",
+    mark: "A1",
+    year: "2012",
+    transmission: "Manual",
+    fuel: "Gasoline",
+  },
+  golf: {
+    img: "golf6",
+    price: 37,
+    model: "golf6",
+    mark: "Volkswagen",
+    year: "2008",
+    transmission: "Manual",
+    fuel: "Diesel",
+  },
+  toyota: {
+    img: "toyotacamry",
+    price: 30,
+    model: "Camry",
+    mark: "Toyota",
+    year: "2006",
+    transmission: "Automatic",
+    fuel: "Hybrid",
+  },
+  bmw: {
+    img: "bmw320",
+    price: 35,
+    model: "320",
+    mark: "BMW",
+    year: "2012",
+    transmission: "Manual",
+    fuel: "Diesel",
+  },
+  mercedes: {
+    img: "benz",
+    price: 50,
+    model: "Benz GLK",
+    mark: "Mercedes",
+    year: "2006",
+    transmission: "Manual",
+    fuel: "Diesel",
+  },
+  passat: {
+    img: "passatcc",
+    price: 25,
+    model: "Passat CC",
+    mark: "Volkswagen",
+    year: "2008",
+    transmission: "Automatic",
+    fuel: "Gasoline",
+  },
+};
+
+export default cars;
--- src/components/Pick.jsx
+++ src/components/Pick.jsx
@@ -1,68 +1,12 @@
 import { useState, useEffect } from "react";
+import cars from "../CarsDetails";
 
 const Pick = () => {
-  const allCars = {
-    audi: {
-      img: "audia1",
-      price: 45,
-      model: "Audi",
-      mark: "A1",
-      year: "2012",
-      transmission: "Manual",
-      fuel: "Gasoline",
-    },
-    golf: {
-      img: "golf6",
-      price: 37,
-      model: "golf6",
-      mark: "Volkswagen",
-      year: "2008",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-    toyota: {
-      img: "toyotacamry",
-      price: 30,
-      model: "Camry",
-      mark: "Toyota",
-      year: "2006",
-      transmission: "Automatic",
-      fuel: "Hybrid",
-    },
-    bmw: {
-      img: "bmw320",
-      price: 35,
-      model: "320",
-      mark: "BMW",
-      year: "2012",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-    mercedes: {
-      img: "benz",
-      price: 50,
-      model: "Benz GLK",
-      mark: "Mercedes",
-      year: "2006",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-    passat: {
-      img: "passatcc",
-      price: 25,
-      model: "Passat CC",
-      mark: "Volkswagen",
-      year: "2008",
-      transmission: "Automatic",
-      fuel: "Gasoline",
-    },
-  };
-
   const [car, setCar] = useState("audi");
-  const [carDetails, setCarDetails] = useState(allCars[car]);
+  const [carDetails, setCarDetails] = useState(cars[car]);
 
   useEffect(() => {
-    setCarDetails(allCars[car] || "");
+    setCarDetails(cars[car] || "");
   }, [car]);
 
   return (
