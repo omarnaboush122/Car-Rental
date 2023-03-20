bc0b402bd916e3c7f563053d31bcd0676f7ee1ce
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-20 09:56:33 +0200

    add first part of aboutcompany section

---
+++ src/components/About-Company.jsx
@@ -0,0 +1,32 @@
+const AboutCompany = () => {
+  return (
+    <section>
+      <div className="max-w-[1480px] mx-auto px-8">
+        <div className="grid grid-cols-1 max-w-full items-center text-center gap-12 my-24 mx-auto">
+          <img
+            src="./images/about/about-main.jpg"
+            alt="main-img"
+            className="w-full h-auto"
+          />
+          <article className="flex flex-col text-Black">
+            <h3 className="font-Rubik text-2xl font-medium mb-3">
+              About Company
+            </h3>
+            <h2 className="text-5xl font-bold">
+              You start the engine and your adventure begins
+            </h2>
+            <p className="text-[#706f7b] font-Rubik mt-5 mb-10">
+              Certain but she but shyness why cottage. Guy the put instrument
+              sir entreaties affronting. Pretended exquisite see cordially the
+              you. Weeks quiet do vexed or whose. Motionless if no to affronting
+              imprudence no precaution. My indulged as disposal strongly
+              attended.
+            </p>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutCompany;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
+import AboutCompany from "../components/About-Company";
 import Intro from "../components/Intro";
 
 const About = () => {
   return (
     <main>
       <Intro title="About" />
+      <AboutCompany />
     </main>
   );
 };
