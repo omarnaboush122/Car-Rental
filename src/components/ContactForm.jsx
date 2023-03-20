6960594710433344b8071e4fe6cb6f569c4c5544
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-20 20:09:33 +0200

    finish the form of contact page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,9 +4,9 @@ const ContactForm = () => {
   return (
     <section>
       <div className="max-w-[1480px] mx-auto px-8">
-        <div className="contact grid grid-cols-1 gap-8 mx-auto py-24 px-5 text-Black text-center">
-          <article className="flex flex-col gap-3 w-[410px] mx-auto mb-5">
-            <h2 className="text-5xl mb-5">Need additional information?</h2>
+        <div className="contact grid grid-cols-1 gap-8 mx-auto py-24 px-5 text-Black text-center lg:grid-cols-2 lg:text-left">
+          <article className="flex flex-col gap-3 w-[410px] mx-auto mb-5 lg:m-0">
+            <h2 className="text-5xl font-bold mb-5">Need additional information?</h2>
             <p className="text-[#706f7b] font-Rubik mb-5 md:text-lg">
               A multifaceted professional skilled in multiple fields of
               research, development as well as a learning specialist. Over 15
@@ -24,6 +24,43 @@ const ContactForm = () => {
               <i className="fa-solid fa-location-dot"></i>
               &nbsp; Belgrade, Serbia
             </Link>
+          </article>
+          <article className="flex flex-col">
+            <form className="flex flex-col">
+              <label htmlFor="name" className="text-lg font-bold mb-3">
+                Full Name
+                <b className="text-Red">*</b>
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="E.g: Joe Shmoe"
+                className="bg-[#f2f2f2] py-4 px-8 mb-6 border-none outline-none"
+              />
+              <label htmlFor="email" className="text-lg font-bold mb-3">
+                Email
+                <b className="text-Red">*</b>
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="youremail@example.com"
+                className="bg-[#f2f2f2] py-4 px-8 mb-6 border-none outline-none"
+              />
+              <label htmlFor="textarea" className="text-lg font-bold mb-3">
+                Email
+                <b className="text-Red">*</b>
+              </label>
+              <textarea
+                id="textarea"
+                placeholder="Write Here.."
+                className="bg-[#f2f2f2] h-[180px] py-4 px-8 mb-6 border-none outline-none resize-none"
+              ></textarea>
+              <button className="bg-Red text-[#fff] text-lg py-4 px-8 font-bold border-2 border-solid border-Red rounded cursor-pointer shadow-md transition-all duration-300 hover:bg-[#fa4226] hover:shadow-lg">
+                <i className="fa-solid fa-envelope-open-text"></i>
+                &nbsp; Send Message
+              </button>
+            </form>
           </article>
         </div>
       </div>
